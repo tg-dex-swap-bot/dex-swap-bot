75bf23c05c68be9b811d6829e12006988f3a0b31
azazuent <azazuent@gmail.com>
2025-05-20 22:15:20 +0300

    Refactor Swap Coffee wrapper, add tests

---
+++ tests/test_get_prepared_transaction.py
@@ -0,0 +1,25 @@
+import random
+
+from src import SwapCoffeeAPI
+from tests.test_get_swap_route import tokens
+
+
+def test_get_prepared_transaction(tokens):
+    amount = 0.00000001
+
+    token_in, token_out = tokens
+
+    route = SwapCoffeeAPI.get_swap_route(
+        token_in["address"]["address"],
+        token_out["address"]["address"],
+        amount,
+        True
+    )
+
+    SwapCoffeeAPI.get_prepared_transaction(
+        "UQCNTO0Nh0Z7QNyRW1BLWfk08f2dAOw4izrx9sO6OUPg4DoV",
+        0.05,
+        route["paths"]
+    )
+
+
